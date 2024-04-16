f269bc10a6e0b3f31ba4c39582f12a7219ae6bf9
victor297 <davidvictor297@gmail.com>
2024-04-16 17:30:25 +0100

    latest

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
     this.resetToken = resetToken;
-    this.from = `Trenova Support <${process.env.EMAIL_FROM}>`;
+    this.from = `Trenova Support <officialtrenovaacademy@gmail.com>`;
   }
 
   newTransport() {
